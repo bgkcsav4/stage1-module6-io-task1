4b8b65476a27b8049a3a0950a9023d4d3a60a710
Latsa <bgkcsavvy@gmail.com>
2023-07-25 12:06:24 +0200

    done.

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,56 +2,28 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 
 public class FileReader {
 
-    public static Profile getDataFromFile(File file) throws IOException {
+    public static Profile getDataFromFile(File file) {
         String content;
-        FileInputStream fileInputStream = null;
-
-        try {
-            fileInputStream = new FileInputStream(file.getPath());
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        } finally {
-            if (fileInputStream != null) {
-                try {
-                    fileInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        try {
-            fileInputStream = new FileInputStream(file.getPath());
-        } catch (FileNotFoundException e) {
-            fileInputStream.close();
-            throw new RuntimeException(e);
-
-        }
-
-        byte[] bytes = new byte[1024];
-        int bts;
         String[] res = new String[0];
-        while (true) {
-            try {
-                if ((bts = fileInputStream.read(bytes)) == -1) break;
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+
+        try (FileInputStream fileInputStream = new FileInputStream(file.getPath())) {
+            byte[] bytes = new byte[1024];
+            int bts;
+            while ((bts = fileInputStream.read(bytes)) != -1) {
+                content = new String(bytes, 0, bts);
+                res = finalContent(content);
             }
-            fileInputStream.close();
-            content = new String(bytes, 0, bts);
-            res = finalContent(content);
-
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-          return new Profile(res[0], Integer.parseInt(res[1]), res[2], Long.parseLong(res[3]));
-    }
 
+        return new Profile(res[0], Integer.parseInt(res[1]), res[2], Long.parseLong(res[3]));
+    }
 
     public static String[] finalContent(String content) {
         String[] finalContent = new String[4];
@@ -66,5 +38,3 @@ public class FileReader {
     }
 
 }
-
-
